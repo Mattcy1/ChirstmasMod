e6f36393803c502bf493c62666ff465119497730
Mattcy <140085165+Mattcy1@users.noreply.github.com>
2024-12-12 13:26:00 +0100

    Add files via upload

---
+++ ChirstmasMod.cs
@@ -0,0 +1,16 @@
+﻿using MelonLoader;
+using BTD_Mod_Helper;
+using ChirstmasMod;
+
+[assembly: MelonInfo(typeof(ChirstmasMod.ChirstmasMod), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+
+namespace ChirstmasMod;
+
+public class ChirstmasMod : BloonsTD6Mod
+{
+    public override void OnApplicationStart()
+    {
+        ModHelper.Msg<ChirstmasMod>("ChirstmasMod loaded!");
+    }
+}
---
+++ ModHelperData.cs
@@ -0,0 +1,13 @@
+﻿namespace ChirstmasMod;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "46.0";
+    public const string Version = "1.0.0";
+    public const string Name = "ChirstmasMod";
+
+    public const string Description = "An empty mod";
+
+    public const string RepoOwner = "Mattcy1"; 
+    public const string RepoName = "ChirstmasMod"; 
+}

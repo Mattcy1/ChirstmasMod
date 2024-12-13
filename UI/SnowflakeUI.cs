c3d748aebdd109c1e6db069ad9cd7a86433aaf81
Mattcy <140085165+Mattcy1@users.noreply.github.com>
2024-12-14 00:06:08 +0100

    Update SnowflakeUI.cs

--- UI/SnowflakeUI.cs
+++ UI/SnowflakeUI.cs
@@ -3,6 +3,7 @@ using BTD_Mod_Helper.Api.Enums;
 using BTD_Mod_Helper.Extensions;
 using ChirstmasMod;
 using Il2CppAssets.Scripts.Unity.UI_New.InGame;
+using Il2CppAssets.Scripts.Unity.UI_New.Popups;
 using MelonLoader;
 using UnityEngine;
 
@@ -35,6 +36,13 @@ public class Snowflake
                 image.AddText(new("Title_", 0, -200, 350, 200), $"Snowflakes: " + Values.snowflake, 60);
                 var image1 = panel.AddImage(new("Image_", 450, 150, 275), VanillaSprites.GiftRed);
                 image1.AddText(new("Title_", 0, -200, 350, 200), $"Gift: " + Values.gift + " / 5", 60);
+                var Sell = panel.AddButton(new("Button_", 0, 0, 300, 150), VanillaSprites.GreenBtnLong, new System.Action(() =>
+                {
+                    InGame.instance.AddCash(25 * Values.snowflake);
+                    PopupScreen.instance?.ShowOkPopup($"You sold all your snowflakes for: {25 * Values.snowflake}$");
+                    Values.snowflake = 0;
+                }));
+                Sell.AddText(new("Title_", 0, 0, 300, 150), "Sell snowflakes", 40);
             }
         }
     }

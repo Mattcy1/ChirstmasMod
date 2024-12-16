8207963bdd7ba723d4a5745f1cdebc6150c3156f
DarkTerraYT <liamwolfguy@gmail.com>
2024-12-16 18:13:28 -0500

    Ice moab (didn't test fully)

--- obj/Debug/net6.0/ChirstmasMod.AssemblyInfo.cs
+++ obj/Debug/net6.0/ChirstmasMod.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TemplateMod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d6d86bb8906cfbc4d28dbafda1514334c35a580")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d9ebf1001c4a9709dbde3b35b982c70e95976e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TemplateMod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TemplateMod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

b6ae1799d833d2058a31c38215856a377f04497f
DarkTerraYT <liamwolfguy@gmail.com>
2024-12-18 16:59:22 -0500

    Reapply "Stuffs"
    This reverts commit 6b6ed1b3cd23e1a81697bf18723974df5766e793.

--- UI/StoryUI.cs
+++ UI/StoryUI.cs
@@ -29,7 +29,7 @@ public class Story
         ElfLordWant,
         SnowMoab,
         CrumblyIcon,
-        CookieMonsterIcon,
+        AngryCookieMonsterIcon,
         GrinchAngryIcon,
         Player,
         PlayerNoWay,

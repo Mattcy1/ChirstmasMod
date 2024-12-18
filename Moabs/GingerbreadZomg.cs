0ade1a2fa174cbae809b64f7012f8e4357655ea2
Mattcy <brocourtmattheo@gmail.com>
2024-12-18 21:52:29 +0100

    Unexecpted crash stack overflow or some shit @lerp fix ty

---
+++ Moabs/GingerbreadZomg.cs
@@ -0,0 +1,42 @@
+﻿using BTD_Mod_Helper.Api.Bloons;
+using BTD_Mod_Helper.Api.Display;
+using BTD_Mod_Helper.Extensions;
+using Il2CppAssets.Scripts.Models.Bloons;
+using Il2CppAssets.Scripts.Unity.Display;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Il2CppAssets.Scripts.Models.Bloons.Behaviors;
+using Il2CppAssets.Scripts.Unity;
+using TemplateMod.Bloons;
+using UnityEngine;
+
+namespace TemplateMod.Moabs
+{
+    public class GingerbreadZomg : ModBloon
+    {
+        public override string BaseBloon => BloonType.sZomg;
+
+        public override void ModifyBaseBloonModel(BloonModel bloonModel)
+        {
+            bloonModel.RemoveAllChildren();
+            bloonModel.AddToChildren<GingerbreadZomg>(4);
+            bloonModel.maxHealth = 3600;
+        }
+
+        public class GingerbreadZomg0 : ModBloonDisplay<GingerbreadZomg>
+        {
+            public override string BaseDisplay => Game.instance.model.GetBloon("Zomg").GetDisplayGUID();
+
+            public override void ModifyDisplayNode(UnityDisplayNode node)
+            {
+                foreach(var renderer in node.GetMeshRenderers())
+                {
+                    renderer.SetMainTexture(GetTexture(Name)); 
+                }
+            }
+        }
+    }
+}
---
+++ Moabs/SnowZomg.cs
@@ -0,0 +1,43 @@
+﻿using BTD_Mod_Helper.Api.Bloons;
+using BTD_Mod_Helper.Api.Display;
+using BTD_Mod_Helper.Extensions;
+using Il2CppAssets.Scripts.Models.Bloons;
+using Il2CppAssets.Scripts.Unity.Display;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Il2CppAssets.Scripts.Models.Bloons.Behaviors;
+using Il2CppAssets.Scripts.Unity;
+using TemplateMod.Bloons;
+using UnityEngine;
+
+namespace TemplateMod.Moabs
+{
+    public class SnowZomg : ModBloon
+    {
+        public override string BaseBloon => BloonType.sZomg;
+
+        public override void ModifyBaseBloonModel(BloonModel bloonModel)
+        {
+            bloonModel.RemoveAllChildren();
+            bloonModel.AddToChildren<SnowBfb>(4);
+            bloonModel.maxHealth = 3500;
+            bloonModel.bloonProperties = Il2Cpp.BloonProperties.White;
+        }
+
+        public class SnowZomg0 : ModBloonDisplay<SnowZomg>
+        {
+            public override string BaseDisplay => Game.instance.model.GetBloon("Zomg").GetDisplayGUID();
+
+            public override void ModifyDisplayNode(UnityDisplayNode node)
+            {
+                foreach(var renderer in node.GetMeshRenderers())
+                {
+                    renderer.SetMainTexture(GetTexture(Name)); 
+                }
+            }
+        }
+    }
+}
